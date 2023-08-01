c1d77f198e00b162e8eeeaddd656e761f312aa68
root <admin@example.com>
2023-08-01 19:38:35 +0800

    решение задачи 1.1

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,10 @@
 function factorial(n) {
-  // ваш код...
+  let tmp = 1;
+  if(n === +n && n % 1 === 0 && n >= 0) {
+    for (i=n; i>1; i--) tmp *= i;
+  } else {
+    tmp = undefined;
+    console.log('Функция «факториа́л» определена на множестве НЕОТРИЦАТЕЛЬНЫХ ЦЕЛЫХ чисел:\nwikipedia.org/wiki/Factorial');
+  }
+  return tmp;
 }
