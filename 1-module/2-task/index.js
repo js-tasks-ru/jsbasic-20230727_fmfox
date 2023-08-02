1deb2e0722301c04cf50e406f21d2505bc222be4
root <admin@example.com>
2023-08-02 21:14:59 +0800

    решение задачи 1.2

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -3,18 +3,17 @@ function print(text) {
 }
 
 function isValid(name) {
-  if(typeof(name) === 'string' && /^\S*$/.test(name) && name.length > 3 && name != null && name != undefined) {
-    return 1;
-  } else return 0;
+  if(typeof(name) === 'string' && /^\S*$/.test(name) && name.length > 3) return 1;
+  else return 0;
 }
 
 function sayHello() {
   let userName = prompt('Введите ваше имя');
 
   if (isValid(userName)) {
-    print(`Добро пожаловать, ${userName}!`);
+    print(`Welcome back, ${userName}!`);
   } else {
-    print('Имя не соответствует условиям');
+    print('Некорректное имя');
   }
 }
 
