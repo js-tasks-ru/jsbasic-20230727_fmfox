ff1b915e3791413b5f62faa09ced611c7b242c88
root <admin@example.com>
2023-08-02 19:30:57 +0800

    решение задачи 1.2

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -1,24 +1,18 @@
-/**
- * Эту функцию трогать не нужно
- */
 function print(text) {
   console.log(text);
 }
 
-/**
- * Эту функцию нужно поменять так,
- * чтобы функция sayHello работала корректно
- */
 function isValid(name) {
-  // ваш код...
+  if(typeof(name) === 'string' && /^\S*$/.test(name) && name.length > 3) return 1;
+  else return 0;
 }
 
 function sayHello() {
   let userName = prompt('Введите ваше имя');
 
   if (isValid(userName)) {
-    print(`Welcome back, ${userName}!`);
+    print(`Добро пожаловать, ${userName}!`);
   } else {
-    print('Некорректное имя');
+    print('Имя не соответствует условиям');
   }
 }
