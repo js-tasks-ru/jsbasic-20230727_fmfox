b6fe381f28e622253fac1172eefc005d7c0017c8
root <admin@example.com>
2023-08-12 18:42:54 +0800

    решение задачи 1.3

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,3 +1,8 @@
 function ucFirst(str) {
-  // ваш код...
+  if(typeof(str)==='string') {
+    if(str=='') return str;
+    let str_tmp = '';
+    for(i = 1 ; i < str.length ; i++) str_tmp += str[i];
+    return str[0].toUpperCase() + str_tmp;
+  } else console.log('на вход подана НЕ строка');
 }
