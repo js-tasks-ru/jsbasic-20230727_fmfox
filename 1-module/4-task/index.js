f9640e899e115ab12852eb4b694f5f1cd8c7254c
root <admin@example.com>
2023-08-13 23:53:43 +0800

    решение задачи 1.4

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,9 @@
 function checkSpam(str) {
-  // ваш код...
+  if(typeof(str)==='string') {
+    var spamWords = ['1xBet', 'XXX', 'viagra', 'porn', 'scam'];
+    function spamOrNot(word) {
+      return str.toLowerCase().includes(word.toLowerCase());
+    }
+    return spamWords.some(spamOrNot);
+  } else console.log('функция «checkSpam» работает только со строками');
 }
