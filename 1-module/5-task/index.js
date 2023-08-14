9f13acded6fdf19a5a97e010f39226109488853a
root <admin@example.com>
2023-08-14 23:42:45 +0800

    решение задачи 1.5

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -2,7 +2,7 @@ function truncate(str, maxlength) {
   if(typeof(str)==='string') {
     if(typeof(maxlength)==='number' && !isNaN(maxlength) && maxlength > 0 && maxlength % 1 === 0) {
        if(str.length <= maxlength) return str;
-       else return str.slice(0, maxlength) + "…";
+       else return str.slice(0, maxlength - 1) + "…";
     } else console.log('второй аргумент должен быть натуральным числом');
   } else console.log('функция «truncate» работает только со строками');
 }
