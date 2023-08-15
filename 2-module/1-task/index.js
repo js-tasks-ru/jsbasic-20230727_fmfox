b4121bc49275a821e038a7e1e5fba9eedcbdc2fb
root <admin@example.com>
2023-08-15 21:30:08 +0800

    решение задачи 2.1

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,14 @@
-function sumSalary(salaries) {
-  // ваш код...
+function sumSalary(obj) {
+  let sumzp = 0;
+  let i = 0;
+  for (key in obj) {
+    let val = obj[key];
+    if(typeof(val) === 'number' && !isNaN(val) && isFinite(val)) sumzp += val;
+    else i++;
+  }
+  if(Object.keys(obj).length === 0) console.log('Пустой объект');
+  else if(sumzp == 0 && i > 0) console.log('В объекте не найдено зарплат (св-в с конечными числовыми значениями)');
+  else if(sumzp > 0 && 'currency' in obj) console.log(`Сумма всех зарплат: ${sumzp} ${obj['currency']}`);
+  else console.log(`Сумма всех зарплат: ${sumzp}`);
+  return sumzp;
 }
