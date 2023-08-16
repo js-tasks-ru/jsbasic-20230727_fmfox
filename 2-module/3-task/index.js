4d73e0bf313ea57b728eb913b9fe9986cb810d6a
root <admin@example.com>
2023-08-17 03:34:46 +0800

    решение задачи 1.3

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,13 +1,17 @@
 let calculator = {
   read(a, b) {
     if( typeof(a)==='number' && !isNaN(a) && isFinite(a) ) calculator.a = a;
+    else throw new Error ('Переменные должны быть конечными ЧИСЛАМИ!');
     if( typeof(b)==='number' && !isNaN(b) && isFinite(b) ) calculator.b = b;
+    else throw new Error ('Переменные должны быть конечными ЧИСЛАМИ!');
   },
   sum() {
     if('a' in calculator && 'b' in calculator) return calculator.a + calculator.b;
+    throw new Error ('Сперва нужно передать в функцию 2 числа: read(x, y)');
   },
   mul() {
     if('a' in calculator && 'b' in calculator) return calculator.a * calculator.b;
+    throw new Error ('Сперва нужно передать в функцию 2 числа: read(x, y)');
   }
 };
 
