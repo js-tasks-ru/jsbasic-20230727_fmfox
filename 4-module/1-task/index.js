0de1a9081f7940a27123d92e940ef2a9d81b8432
root <admin@example.com>
2023-08-20 21:08:53 +0800

    решение задач 4.1, 4.2, 4.3, 5.1, 5.2

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,21 @@
-function makeFriendsList(friends) {
-  // ваш код...
+function makeFriendsList(obj) {
+  let tmp = [];
+  let j = 0;
+  friends.forEach( (i) => {
+    if('firstName' in i) {
+      tmp[j] = i.firstName;
+      if('lastName' in i) tmp[j] += ' ' + i.lastName;
+    } else if('lastName' in i) tmp[j] = i.lastName;
+    else throw new Error('в объекте нет ни firstName, ни lastName');
+    j++;
+  });
+  
+  let res = document.createElement('ul');
+	for(let i = 0; i < tmp.length; i++) {
+      let li = document.createElement('li');	// МОЖНО ЛИ
+      li.innerText = `${tmp[i]}`;		// БЫЛО КАК-ТО
+      res.append(li);				// ЭТО ПРОЩЕ СДЕЛАТЬ ?  в одну строку может...
+    }
+  console.log(res);
+  return res;
 }
